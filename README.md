4c38df377fb05253f8d82d63092135877269f009
kjwu00 <kjwu00@stanford.edu>
2020-12-14 14:20:27 -0500

    can send emails now

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Secret Snowflake
 
-Allows a group of friends to have coordinate a secret santa exchange. Takes in a txt file with the participants' name and emails, then 
+Allows a group of friends (>2 people) to have coordinate a secret santa exchange. Takes in a txt file with the participants' name and emails, then creates matches and emails each participant their match.
 
 ## Set-up
 
-## Input file format
+## Usage
+python3 secret_snowflake.py input_file.txt
 
+Each line in the input file should be formatted 'Name, email'. See sample_input.txt for an example input file.
--- secret_snowflake.py
+++ secret_snowflake.py
@@ -1,36 +1,67 @@
 import random
 import sys
 import smtplib
+import os
+from dotenv import load_dotenv
 
-PORT = 587
+load_dotenv()
+GMAIL_USER = os.getenv("GMAIL_USER")
+GMAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
 
 def read_file(in_file):
     name_email = {}
     with open(in_file, 'r') as f:
         for line in f:
+            line = line.strip()
+            if len(line) == 0:
+                pass
+
             parts = line.split(",")
             if len(parts) != 2:
                 raise Exception ("make sure every line in the input file is: name, email")
             name_email[parts[0].strip()] = parts[1].strip()
+
     return name_email
+
 
 def make_pairing(name_email, matches):
     people = list(name_email.keys())
-    if len(people) < 2:
+    if len(people) <= 2:
         raise Exception ("make sure there are more than two people")
 
     random.shuffle(people)
     for i in range(len(people)):
         matches[people[i]] = people[(i+1)%len(people)]
 
+
 def send_emails(name_email, matches):
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
+        # Connect to email server.
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
+        server.login(GMAIL_USER, GMAIL_PASSWORD)
+
+        # Send emails
+        for giver, receiver in matches.items():
+            sent_from = GMAIL_USER
+            to = [name_email[receiver]]
+            subject = "Secret Snowflake"
+            body = "Hi " + giver + "! Get a gift for " + receiver +"."
+
+            email_text = """\
+            From: %s
+            To: %s
+            Subject: %s
+
+            %s
+            """ % (sent_from, ", ".join(to), subject, body)
+            server.sendmail(sent_from, to, email_text)
+
+        server.close()
+                    
     except:
-        print("Was unable to setup server for emaiil.")
-    
-    
+        print("Was unable to send emails.")
+
 
 def main():
     if len(sys.argv) != 2:
@@ -40,6 +71,7 @@ def main():
     matches = {}
     make_pairing(name_email, matches)
     send_emails(name_email, matches)
+
 
 if __name__ == '__main__':
     main()
